fc4f46352636a4b2b7b8497bc675e220f6112e1b
Lyx <liuyingxian@finupgroup.com>
2017-03-20 18:07:59 +0800

    runnable version 1.0

--- cockpit-component/src/main/java/com/github/cockpit/ComponentMain.java
+++ cockpit-component/src/main/java/com/github/cockpit/ComponentMain.java
@@ -6,17 +6,14 @@ import org.springframework.boot.Banner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
-import javax.sql.DataSource;
-import java.util.List;
+import java.util.Arrays;
 
 /**
  * ComponentMain
@@ -43,9 +40,17 @@ public class ComponentMain {
     }
 
     @Bean
-    CommandLineRunner commandLineRunner() {
+    CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return (String... args) -> {
             System.out.println("Component is ready to run...");
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+
             System.out.println("Enjoy ur work!");
         };
     }
